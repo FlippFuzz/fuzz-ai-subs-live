b7f52e6062376595a4a2e4553c09048a81ac720a
FlippFuzz <flippfuzz@gmail.com>
2023-06-11 11:21:14 +0800

    Reverting to faster whisper (again)

--- WhisperCppTranslator.py
+++ WhisperCppTranslator.py
@@ -1,30 +1,30 @@
-from pywhispercpp.model import Model
-from Translator import Translator
-
-
-class WhisperCppTranslator(Translator):
-    """
-    Translate using Whisper Cpp
-    Documentation for the WhisperCpp Binding library can be found at:
-    https://abdeladim-s.github.io/pywhispercpp/#pywhispercpp.constants.AVAILABLE_MODELS
-    """
-    MODEL_SIZE = "large"
-    COMPUTE_TYPE = "int8"
-    BEAM_SIZE = 1
-    TEMPERATURE = 0
-
-    def __init__(self):
-        print("Setting Up WhisperCpp")
-        #self.model = Model(self.MODEL_SIZE, audio_ctx=750)
-        self.model = Model(self.MODEL_SIZE)
-
-    def translate(self, audio_file: str) -> str:
-        translation = ""
-
-        segments = self.model.transcribe(audio_file)
-
-        for segment in segments:
-            translation += segment.text + "\n"
-            print(segment.text)
-
-        return translation
+# from pywhispercpp.model import Model
+# from Translator import Translator
+#
+#
+# class WhisperCppTranslator(Translator):
+#     """
+#     Translate using Whisper Cpp
+#     Documentation for the WhisperCpp Binding library can be found at:
+#     https://abdeladim-s.github.io/pywhispercpp/#pywhispercpp.constants.AVAILABLE_MODELS
+#     """
+#     MODEL_SIZE = "large"
+#     COMPUTE_TYPE = "int8"
+#     BEAM_SIZE = 1
+#     TEMPERATURE = 0
+#
+#     def __init__(self):
+#         print("Setting Up WhisperCpp")
+#         #self.model = Model(self.MODEL_SIZE, audio_ctx=750)
+#         self.model = Model(self.MODEL_SIZE)
+#
+#     def translate(self, audio_file: str) -> str:
+#         translation = ""
+#
+#         segments = self.model.transcribe(audio_file)
+#
+#         for segment in segments:
+#             translation += segment.text + "\n"
+#             print(segment.text)
+#
+#         return translation
